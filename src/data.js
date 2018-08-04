2bc6fbd9a964c3c0717a87e8abd20b37f2e4dd38
AlidaContreras <alidacontreras01@gmail.com>
2018-08-04 15:44:17 -0500

    cada archivo data contiene funciones firebase y main DOM

--- src/data.js
+++ src/data.js
@@ -1,25 +1,65 @@
-window.onload = () => {
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      //Si está registrado,entonces iniciará sesión
-      const loggedIn = document.getElementById('loggedIn');
-      window.location.assign('home.html');
-      loginUser.style.display = "none";
-      registerUser.style.display ="none";
-      alert("Hola, ya puedes difrutar de esta red social");
-    } else {
-      //si no estamos logeados
-      loginUser.style.display = "block";
-      registerUser.style.display ="none";
-      //loggedIn.style.display="none";
-    }
-  });
-  window.createAccount = (user) => {
-    loginUser.style.display = "none";
-    registerUser.style.display = "block";
-  }
+window.login = () => {
+	//Se declaran estas variables para llamar al "id" de email y contraseña de login.html
+	const emailValue = document.getElementById('emailLogin').value;
+	const passwordValue = document.getElementById('passwordLogin').value;
+	if (emailValue && passwordValue !== ""){
+		const filtro = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i ;
+		if(!filtro.test(emailValue)){
+			alert("Por favor digite un e-mail valido")
+		}
+		else{
+			firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+		.then(() => {
+			console.log("Usuario con login exitoso")
+		})
+		.catch((error) => {
+			console.log("error de firebase >" + error.code);
+			console.log("error de firebase ,mensaje >" + error.message);
+			alert("Aun no estas registradx ¿que esperas comienza a registrarte y veras todos los beneficios; o quizas tu contraseña no es correcta 😨");
+		})
+		}
+	}
+	else if(emailValue || passwordValue == ""){
+		alert("Ingrese e-mail y contraseña válida")
+	}
 }
-window.back = () => {
-  loginUser.style.display = "block";
-  registerUser.style.display = "none";
+window.register = () => {
+	let nameRegister = nombreR.value;
+	const emailRegister = emailR.value;
+	const passwordRegister = passwordR.value;
+	if( nameRegister && emailRegister && passwordRegister !== ""){
+		const filtro = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i ;
+		if(!filtro.test(emailRegister)){
+			alert ("Ingrese un e-mail y contraseña válidx")
+		}
+		else{
+			firebase.auth().createUserWithEmailAndPassword(emailRegister,passwordRegister)
+    .then(() =>{
+        alert("Bienvenidx tu registro fue exitoso comencemos 💕");
+    })
+    .catch((error)=>{
+        console.log("error de firebase >" +error.code);
+        console.log("errorfirebase,mensaje >" +error.message);
+        //alert("Usuarix ya esta registrado")
+    });
+		}
+	}
+	else{
+		alert("Ingrese todos los campos con información válida")
+	}
+}
+loginFacebook=()=>{
+	const provider = new firebase.auth.FacebookAuthProvider();
+	//provider.addScope("user_birthday");tiene que pedirle permiso a facebook
+	provider.setCustomParameters({
+			'display': 'popup'
+	});
+	firebase.auth().signInWithPopup(provider)
+	.then(()=>{
+		 console.log("login con facebook");
+	})
+	.catch((error)=>{
+			console.log("error de firebase >"+ error.code);
+			console.log("error de firebase ,mensaje >" +error.message);
+	});
 }
---
+++ src/data2.js
@@ -0,0 +1,8 @@
+// Funcion LogOut = Cerrar Sesion
+logout=()=>{
+	firebase.auth().signOut()
+	.then(()=>{
+			window.location.assign("login.html");
+	})
+	.cath();
+}
