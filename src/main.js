91312e9dccc3e7f86b0f5928f4f72fa2f30fea83
LesAndreaCarretero <andrea.carretero2097@gmail.com>
2018-08-06 12:48:48 -0500

    Autenticación con google -todos las cuentas válidas

--- src/main.js
+++ src/main.js
@@ -22,3 +22,9 @@ window.back = () => {
   loginUser.style.display = "block";
   registerUser.style.display = "none";
 }
+/* Aquí se reconocerá los id de los  botones
+  const loginG= document.getElementById("loginGoogleh");
+  loginG.addEventListener('click',event) =() =>{
+   loginGoogle();
+  }
+ */
--- src/main2.js
+++ src/main2.js
@@ -1,5 +1,6 @@
 //Inicializar nav-parte superior
-document.addEventListener('DOMContentLoaded', function() {
+/* document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.sidenav');
 	var instances = M.Sidenav.init(elems, options);
 });
+ */
